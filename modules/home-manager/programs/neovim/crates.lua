38256818548493dfd827780bad1b6f84f8f27da4
Samuel Joli <samuel.joli.ftn@gmail.com>
2025-02-25 22:16:06 -0500

    feat(neovim): enable lsp and cmp for nvim-crates

--- modules/home-manager/programs/neovim/crates.lua
+++ modules/home-manager/programs/neovim/crates.lua
@@ -1,1 +1,21 @@
-require("crates").setup()
+require("crates").setup({
+	lsp = {
+		enabled = true,
+		-- on_attach = function (client, bufnr)
+		--
+		-- end
+		actions = true,
+		completion = true,
+		hover = true,
+	},
+	completion = {
+		crates = {
+			enabled = true,
+			max_results = 4,
+			min_chars = 3,
+		},
+		cmp = {
+			enabled = true,
+		},
+	},
+})
