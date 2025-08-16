630081b909d3e3398bf27d4ef938f2a14351cab7
Samuel Joli <samuel.joli.ftn@gmail.com>
2025-08-16 16:45:57 -0400

    feat(nix): install nh

--- modules/home-manager/programs/default.nix
+++ modules/home-manager/programs/default.nix
@@ -20,6 +20,13 @@
     };
   };
 
+  nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    # flake = "/home/user/my-nixos-config"; # sets NH_OS_FLAKE variable for you
+  };
+
   neovim = import ./neovim/default.nix {
     inherit pkgs;
     inherit inputs;
