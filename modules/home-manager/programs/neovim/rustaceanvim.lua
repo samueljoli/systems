a9d39b0d20013eb430e05c650b557d2f2a18bde6
Samuel Joli <samuel.joli.ftn@gmail.com>
2026-04-25 17:13:51 -0400

    chore: install latest rustaceanvim

--- modules/home-manager/programs/neovim/rustaceanvim.lua
+++ modules/home-manager/programs/neovim/rustaceanvim.lua
@@ -8,8 +8,24 @@ vim.g.rustaceanvim = {
 		default_settings = {
 			-- rust-analyzer language server configuration
 			["rust-analyzer"] = {
+				cachePriming = {
+					enable = false,
+				},
+				cargo = {
+					allTargets = false,
+				},
+				check = {
+					allTargets = false,
+				},
 				files = {
-					excludeDirs = { ".direnv" },
+					excludeDirs = {
+						".direnv",
+						".git",
+						"node_modules",
+						"result",
+						"result-*",
+						"target",
+					},
 				},
 				diagnostic = {
 					refreshSupport = true,
